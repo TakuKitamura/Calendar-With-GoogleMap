17f0b88f40053558eb04af74490fc29ae2e178ab
喜多村卓 <is0352pp@ed.ritsumei.ac.jp>
2017-10-29 13:57:03 +0900

    departureTimeを元に、日付を選択したときに表示する予定を変更。departureTimeを元にソート

--- Calender/ViewController.swift
+++ Calender/ViewController.swift
@@ -59,9 +59,9 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         // Do any additional setup after loading the view, typically from a nib.
         
         // データベース初期化
-        if let fileURL = Realm.Configuration.defaultConfiguration.fileURL {
-            try! FileManager.default.removeItem(at: fileURL)
-        }
+//        if let fileURL = Realm.Configuration.defaultConfiguration.fileURL {
+//            try! FileManager.default.removeItem(at: fileURL)
+//        }
         
         locationManager = CLLocationManager() // インスタンスの生成
         locationManager.delegate = self // CLLocationManagerDelegateプロトコルを実装するクラスを指定する
